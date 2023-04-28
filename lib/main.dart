f4748860d294e6c76f4225b2ef16603c5ad5b48a
Lans12345 <olanalans12345@gmail.com>
2023-04-28 12:28:24 +0800

    added firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:violation_system/screens/home_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'violation-system',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
