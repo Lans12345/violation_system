6f0f7656449c96ec9fae58c8a2e711b33c0721cc
Lans12345 <olanalans12345@gmail.com>
2023-04-28 18:31:43 +0800

    added request for approval page

--- lib/screens/views/admin/request_list.dart
+++ lib/screens/views/admin/request_list.dart
@@ -16,6 +16,59 @@ class RequestListScreen extends StatelessWidget {
             text: 'Request for Approval', fontSize: 18, color: Colors.white),
         centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        child: DataTable(columns: [
+          DataColumn(
+            label: TextBold(text: 'Officer', fontSize: 16, color: Colors.black),
+          ),
+          DataColumn(
+            label: TextBold(text: 'Details', fontSize: 16, color: Colors.black),
+          ),
+          DataColumn(
+            label: TextBold(text: '', fontSize: 0, color: Colors.black),
+          ),
+        ], rows: [
+          for (int i = 0; i < 100; i++)
+            DataRow(cells: [
+              DataCell(
+                TextBold(
+                    text: 'Name of Officer', fontSize: 12, color: Colors.grey),
+              ),
+              DataCell(
+                TextRegular(
+                    text: 'Details of Request',
+                    fontSize: 12,
+                    color: Colors.grey),
+              ),
+              DataCell(
+                SizedBox(
+                  width: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.check_box_outlined,
+                          color: Colors.green,
+                          size: 32,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.highlight_remove_outlined,
+                          color: Colors.red,
+                          size: 32,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ])
+        ]),
+      ),
     );
   }
 }
