bd2865857ef99322e319334bab22b1215c353d74
Lans12345 <olanalans12345@gmail.com>
2023-04-28 19:07:21 +0800

    added offier home page

--- lib/screens/views/officer/officer_home_screen.dart
+++ lib/screens/views/officer/officer_home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../widgets/drawer_widget.dart';
 import '../../../widgets/text_widget.dart';
 
 class OfficerHomeScreen extends StatelessWidget {
@@ -26,6 +25,54 @@ class OfficerHomeScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            TextBold(
+                text: 'Welcome Offier\n      John Doe!',
+                fontSize: 32,
+                color: Colors.black),
+            const SizedBox(
+              height: 50,
+            ),
+            TextRegular(
+                text: 'Violation Records', fontSize: 18, color: Colors.grey),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: SizedBox(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                      child: Card(
+                        child: ListTile(
+                          title: TextBold(
+                              text: 'Name of Violation',
+                              fontSize: 14,
+                              color: Colors.black),
+                          subtitle: TextRegular(
+                              text: 'Person who commited the violation',
+                              fontSize: 11,
+                              color: Colors.grey),
+                          trailing: TextRegular(
+                              text: 'Date and Time',
+                              fontSize: 12,
+                              color: Colors.grey),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
