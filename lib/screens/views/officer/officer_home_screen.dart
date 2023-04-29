aeaf23444246af7179394352c8f7732186656dd7
Lans12345 <olanalans12345@gmail.com>
2023-04-29 12:38:38 +0800

    added add violation dialog

--- lib/screens/views/officer/officer_home_screen.dart
+++ lib/screens/views/officer/officer_home_screen.dart
@@ -5,6 +5,7 @@ import 'package:violation_system/screens/views/officer/tabs/active_tab.dart';
 import 'package:violation_system/screens/views/officer/tabs/home_tab.dart';
 import 'package:violation_system/screens/views/officer/tabs/profile_tab.dart';
 import 'package:violation_system/screens/views/officer/tabs/toplist_tab.dart';
+import 'package:violation_system/widgets/textfield_widget.dart';
 
 import '../../../widgets/text_widget.dart';
 
@@ -31,6 +32,16 @@ class _OfficerHomeScreenState extends State<OfficerHomeScreen> {
     });
   }
 
+  final platenumberController = TextEditingController();
+  final vehicledescriptionController = TextEditingController();
+  final locationController = TextEditingController();
+
+  var _dropValue1 = 0;
+
+  List vehicles = ['Car', 'Motorcycle', 'Bus', 'Jeep', 'Van'];
+
+  late String vehicle = 'Car';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +49,119 @@ class _OfficerHomeScreenState extends State<OfficerHomeScreen> {
           ? FloatingActionButton(
               backgroundColor: const Color(0xff6571E0),
               child: const Icon(Icons.add),
-              onPressed: () {})
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: TextBold(
+                            text: 'Adding Violation',
+                            fontSize: 18,
+                            color: Colors.black),
+                        content: StatefulBuilder(builder: (context, setState) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: TextRegular(
+                                    text: 'Vehicle Type',
+                                    fontSize: 12,
+                                    color: Colors.black),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 300,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Colors.white,
+                                    border: Border.all(color: Colors.black)),
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                  child: DropdownButton(
+                                      dropdownColor: Colors.white,
+                                      focusColor: Colors.white,
+                                      value: _dropValue1,
+                                      items: [
+                                        for (int i = 0;
+                                            i < vehicles.length;
+                                            i++)
+                                          DropdownMenuItem(
+                                            onTap: (() {
+                                              vehicle = vehicles[i];
+                                            }),
+                                            value: i,
+                                            child: Row(
+                                              children: [
+                                                TextRegular(
+                                                    text: '${vehicles[i]}',
+                                                    fontSize: 14,
+                                                    color: Colors.black),
+                                              ],
+                                            ),
+                                          ),
+                                      ],
+                                      onChanged: ((value) {
+                                        setState(() {
+                                          _dropValue1 =
+                                              int.parse(value.toString());
+                                        });
+                                      })),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFieldWidget(
+                                  label: 'Plate Number',
+                                  controller: platenumberController),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFieldWidget(
+                                  label: 'Vehicle Description',
+                                  controller: vehicledescriptionController),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFieldWidget(
+                                  label: 'Location',
+                                  controller: locationController),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                            ],
+                          );
+                        }),
+                        actions: [
+                          MaterialButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: TextRegular(
+                                text: 'Close',
+                                color: Colors.grey,
+                                fontSize: 12),
+                          ),
+                          MaterialButton(
+                            onPressed: () async {
+                              locationController.clear();
+                              vehicledescriptionController.clear();
+                              platenumberController.clear();
+                              Navigator.of(context).pop(true);
+                            },
+                            child: TextBold(
+                                text: 'Continue',
+                                color: Colors.black,
+                                fontSize: 14),
+                          ),
+                        ],
+                      );
+                    });
+              },
+            )
           : const SizedBox(),
       appBar: AppBar(
         leading: Padding(
