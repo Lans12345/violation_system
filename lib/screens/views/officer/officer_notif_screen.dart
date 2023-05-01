5d1423e3e3e6839d0cbeaa1d7c3247ceaa741727
Lans12345 <olanalans12345@gmail.com>
2023-05-01 19:16:53 +0800

    added data on notif ist

--- lib/screens/views/officer/officer_notif_screen.dart
+++ lib/screens/views/officer/officer_notif_screen.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 import '../../../widgets/text_widget.dart';
 
 class OfficerNotifScreen extends StatelessWidget {
@@ -23,22 +24,53 @@ class OfficerNotifScreen extends StatelessWidget {
             TextBold(text: 'Notifications', fontSize: 24, color: Colors.white),
         centerTitle: true,
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-          child: Card(
-            child: ListTile(
-              leading: const Icon(Icons.notifications_active_sharp),
-              title: TextBold(
-                  text: 'Name of Notification',
-                  fontSize: 14,
-                  color: Colors.black),
-              subtitle: TextRegular(
-                  text: 'Date and Time', fontSize: 12, color: Colors.grey),
-            ),
-          ),
-        );
-      }),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('Notifs')
+              // .where('officerId',
+              //     isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+              .orderBy('dateTime')
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return const Center(child: Text('Error'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.black,
+                )),
+              );
+            }
+
+            final data = snapshot.requireData;
+            return ListView.builder(
+                itemCount: data.docs.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                    child: Card(
+                      child: ListTile(
+                        leading: const Icon(Icons.notifications_active_sharp),
+                        title: TextBold(
+                            text: data.docs[index]['message'],
+                            fontSize: 14,
+                            color: Colors.black),
+                        subtitle: TextRegular(
+                            text: DateFormat.yMMMd()
+                                .add_jm()
+                                .format(data.docs[index]['dateTime'].toDate()),
+                            fontSize: 12,
+                            color: Colors.grey),
+                      ),
+                    ),
+                  );
+                });
+          }),
     );
   }
 }
