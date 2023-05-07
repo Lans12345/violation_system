9494c1c62190d570ede0a2cd17855132abac00fa
Lans12345 <olanalans12345@gmail.com>
2023-05-08 07:20:11 +0800

    added admin approval func

--- lib/screens/views/admin/request_list.dart
+++ lib/screens/views/admin/request_list.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:violation_system/widgets/toast_widget.dart';
 
 import '../../../widgets/drawer_widget.dart';
 import '../../../widgets/text_widget.dart';
@@ -17,57 +19,96 @@ class RequestListScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: DataTable(columns: [
-          DataColumn(
-            label: TextBold(text: 'Officer', fontSize: 16, color: Colors.black),
-          ),
-          DataColumn(
-            label: TextBold(text: 'Details', fontSize: 16, color: Colors.black),
-          ),
-          DataColumn(
-            label: TextBold(text: '', fontSize: 0, color: Colors.black),
-          ),
-        ], rows: [
-          for (int i = 0; i < 100; i++)
-            DataRow(cells: [
-              DataCell(
-                TextBold(
-                    text: 'Name of Officer', fontSize: 12, color: Colors.grey),
-              ),
-              DataCell(
-                TextRegular(
-                    text: 'Details of Request',
-                    fontSize: 12,
-                    color: Colors.grey),
-              ),
-              DataCell(
-                SizedBox(
-                  width: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.check_box_outlined,
-                          color: Colors.green,
-                          size: 32,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.highlight_remove_outlined,
-                          color: Colors.red,
-                          size: 32,
-                        ),
-                      ),
-                    ],
-                  ),
+        child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('Violations')
+                .where('status', isEqualTo: 'Pending')
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                print(snapshot.error);
+                return const Center(child: Text('Error'));
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Center(
+                      child: CircularProgressIndicator(
+                    color: Colors.black,
+                  )),
+                );
+              }
+
+              final data = snapshot.requireData;
+              return DataTable(columns: [
+                DataColumn(
+                  label: TextBold(
+                      text: 'Officer', fontSize: 16, color: Colors.black),
                 ),
-              )
-            ])
-        ]),
+                DataColumn(
+                  label: TextBold(
+                      text: 'Details', fontSize: 16, color: Colors.black),
+                ),
+                DataColumn(
+                  label: TextBold(text: '', fontSize: 0, color: Colors.black),
+                ),
+              ], rows: [
+                for (int i = 0; i < data.docs.length; i++)
+                  DataRow(cells: [
+                    DataCell(
+                      TextBold(
+                          text: 'Name of Officer',
+                          fontSize: 12,
+                          color: Colors.grey),
+                    ),
+                    DataCell(
+                      TextRegular(
+                          text: 'Details of Request',
+                          fontSize: 12,
+                          color: Colors.grey),
+                    ),
+                    DataCell(
+                      SizedBox(
+                        width: 100,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: () async {
+                                await FirebaseFirestore.instance
+                                    .collection('Violations')
+                                    .doc(data.docs[i].id)
+                                    .update({'status': 'Accepted'});
+                                showToast('Request accepted!');
+                              },
+                              icon: const Icon(
+                                Icons.check_box_outlined,
+                                color: Colors.green,
+                                size: 32,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () async {
+                                await FirebaseFirestore.instance
+                                    .collection('Violations')
+                                    .doc(data.docs[i].id)
+                                    .update({'status': 'Rejected'});
+                                showToast('Request rejected!');
+                              },
+                              icon: const Icon(
+                                Icons.highlight_remove_outlined,
+                                color: Colors.red,
+                                size: 32,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ])
+              ]);
+            }),
       ),
     );
   }
