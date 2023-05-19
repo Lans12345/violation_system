794dc9f3e2f0c09c2f1aed8ba8158a28b7019e84
Lans12345 <olanalans12345@gmail.com>
2023-05-19 11:27:08 +0800

    wip violation data

---
+++ lib/data/fines.dart
@@ -0,0 +1,70 @@
+List<Map<String, dynamic>> pujFines = [
+  {
+    'offense': 'Illegal Parking',
+    'fine': 200,
+  },
+  {
+    'offense': 'Disregarding Traffic Sign/Light',
+    'fine': 100,
+  },
+  {
+    'offense': 'Arrogant/Discourteous Driver',
+    'fine': 200,
+  },
+  {
+    'offense': 'Reckless Driving',
+    'fine': 200,
+  },
+  {
+    'offense': 'No OR/CR',
+    'fine': 300,
+  },
+  {
+    'offense': 'No Driver’s License Carried',
+    'fine': 300,
+  },
+  {
+    'offense': 'Trip Cutting',
+    'fine': 500,
+  },
+  {
+    'offense': 'No City Sticker',
+    'fine': 300,
+  },
+  {
+    'offense': 'No Body Number',
+    'fine': 200,
+  },
+  {
+    'offense': 'Wearing Slipper/Sando',
+    'fine': 150,
+  },
+  {
+    'offense': 'Overloading',
+    'fine': 200,
+  },
+  {
+    'offense': 'Out of line',
+    'fine': 200,
+  },
+  {
+    'offense': 'Out of Route',
+    'fine': 1000,
+  },
+  {
+    'offense': 'Establishing Terminal',
+    'fine': 200,
+  },
+  {
+    'offense': 'Refusal to Convey Passenger',
+    'fine': 500,
+  },
+  {
+    'offense': 'Obstruction',
+    'fine': 100,
+  },
+  {
+    'offense': 'Entering Prohibited Zone',
+    'fine': 100,
+  },
+];
